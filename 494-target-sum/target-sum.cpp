08292bc5034d4905171bc511c9c08b0d905826d6
Om Biradar <ombiradar04@gmail.com>
2025-09-22 03:56:46 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (65.42%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,3 +1,27 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+//
+// class Solution {
+// public:
+//   long long cnt = 0, TARGET;
+//   void solve(vector<int> &nums, long long cur, int idx) {
+//     if (idx == nums.size()) {
+//       if (cur == TARGET)
+//         cnt++;
+//       return;
+//     }
+//     solve(nums, cur + nums[idx], idx + 1);
+//     solve(nums, cur - nums[idx], idx + 1);
+//   }
+//   int findTargetSumWays(vector<int> &nums, int target) {
+//     // the length of nums is just 20 at max
+//     // bit mask dp
+//     TARGET = target;
+//     solve(nums, 0, 0);
+//     return cnt;
+//   }
+// };
+
 #include <bits/stdc++.h>
 using namespace std;
 

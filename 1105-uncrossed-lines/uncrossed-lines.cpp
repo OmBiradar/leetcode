695756878a748b1d09711eb518d3607c14c2fff2
Om Biradar <ombiradar04@gmail.com>
2025-09-22 22:42:22 +0530

    Time: 3 ms (90.86%) | Memory: 16.5 MB (69.78%) - LeetSync

---
+++ 1105-uncrossed-lines/uncrossed-lines.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  int maxUncrossedLines(vector<int> &nums1, vector<int> &nums2) {
+    vector<vector<int>> dp(nums1.size() + 1, vector<int>(nums2.size() + 1, 0));
+    // essentially LCS question if you see, int instead of chars
+    for (int i = 1; i <= nums1.size(); i++)
+      for (int j = 1; j <= nums2.size(); j++)
+        if (nums1[i - 1] == nums2[j - 1])
+          dp[i][j] = max(dp[i][j], 1 + dp[i - 1][j - 1]);
+        else
+          dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+
+    return dp[nums1.size()][nums2.size()];
+  }
+};

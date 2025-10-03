6126af0381dcd5d4916311d60e2c127aa2563c8f
Om Biradar <ombiradar04@gmail.com>
2025-10-03 19:01:55 +0530

    Time: 20 ms (75.82%) | Memory: 17.9 MB (73.14%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,21 +1,23 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
 class Solution {
 public:
-  int coinChange(vector<int> &coins, int amount) {
-    vector<long long> dp(amount + 1, -1);
-    dp[0] = 0;
-    for (int i = 0; i <= amount; i++) {
-      if (dp[i] == -1)
-        continue;
-      for (auto c : coins)
-        if (1LL * c + i <= amount)
-          if (dp[c + i] == -1)
-            dp[c + i] = 1 + dp[i];
-          else
-            dp[c + i] = min(dp[c + i], 1 + dp[i]);
+    int coinChange(vector<int> &coins, int amount) {
+        vector<int> dp(amount + 1, -1);
+        dp[0] = 0;
+        for (int i = 0; i <= amount; i++) {
+            if (dp[i] == -1)
+                continue;
+            for (auto c : coins) {
+                if (1LL * c + i <= amount) {
+                    if (dp[c+i] == -1)
+                        dp[c+i] = 1 + dp[i];
+                    else
+                        dp[c + i] = min(dp[c + i], 1 + dp[i]);
+                }
+            }
+        }
+        return dp[amount];
     }
-    return dp[amount] >= INT_MAX ? -1 : dp[amount];
-  }
 };

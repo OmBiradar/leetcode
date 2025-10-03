56c40b499dc9f5ba08705af25f4a8d42f03b83b0
Om Biradar <ombiradar04@gmail.com>
2025-10-03 08:36:41 +0530

    Time: 19 ms (51.43%) | Memory: 12.3 MB (39.02%) - LeetSync

---
+++ 664-strange-printer/strange-printer.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+class Solution {
+public:
+    int strangePrinter(string s) {
+        int n = s.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for (int i = 0; i < n; i++)
+            dp[i][i] = 1;
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+                if (s[i] == s[j])
+                    dp[i][j] = dp[i][j-1];
+                else {
+                    dp[i][j] = INT_MAX;
+                    for (int k = i; k < j; k++) {
+                        dp[i][j] = min(dp[i][j], dp[i][k] + dp[k+1][j]);
+                    }
+                }
+            }
+        }
+        return dp[0][n-1];
+    }
+};

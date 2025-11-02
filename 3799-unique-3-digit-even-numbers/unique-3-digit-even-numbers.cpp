be0b8ec4c2899aef1dbe1ffa118fa694c233d868
Om Biradar <ombiradar04@gmail.com>
2025-11-02 15:54:01 +0530

    Time: 0 ms (100.00%) | Memory: 28.5 MB (100.00%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
@@ -1,29 +1,25 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
 class Solution {
 public:
-    int totalNumbers(vector<int>& digits) {
-        vector<int> cnt(10, 0);
-        for (int d : digits) cnt[d]++;
-
-        int ans = 0;
-        // Fix the ones place as an even digit
-        for (int c = 0; c <= 9; ++c) {
-            if (c % 2 != 0 || cnt[c] == 0) continue; // must be even and available
-            cnt[c]--;
-            // Choose hundreds digit (no leading zero)
-            for (int h = 1; h <= 9; ++h) {
-                if (cnt[h] == 0) continue;
-                cnt[h]--;
-                // Choose tens digit (any digit available)
-                for (int t = 0; t <= 9; ++t) {
-                    if (cnt[t] > 0) ans++;
-                }
-                cnt[h]++;
-            }
-            cnt[c]++;
-        }
-        return ans;
-    }
+	int totalNumbers(vector<int>& digits) {
+		vector<int> cnt(10, 0);
+		for (int d : digits) cnt[d]++;
+		int ans = 0;
+		for (int c = 0; c <= 9; c++) {
+			if (c % 2 == 1 || cnt[c] == 0) continue;
+			cnt[c]--;
+			for (int h = 1; h <= 9; h++) {
+				if (cnt[h] == 0) continue;
+				cnt[h]--;
+				for (int t = 0; t <= 9; t++) {
+					if (cnt[t] > 0) ans++;
+				}
+				cnt[h]++;
+			}
+			cnt[c]++;
+		}
+		return ans;
+	}
 };
